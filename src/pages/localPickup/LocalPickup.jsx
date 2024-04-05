d081c1eb5185369bf50872285ae6065344874c4e
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-04-05 11:57:04 +0200

    resolution de conflit et debus de input item

--- src/pages/localPickup/LocalPickup.jsx
+++ src/pages/localPickup/LocalPickup.jsx
@@ -1,5 +1,11 @@
+import './LocalPickup.css';
+
 const LocalPickup = () => {
-  return <div>LocalPickup page</div>;
+  return (
+    <div className='localPickup'>
+      <p>Page Local Pickup</p>
+    </div>
+  );
 };
 
 export default LocalPickup;
--- src/pages/route/Rout.jsx
+++ src/pages/route/Rout.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navigation from '../../components/navigation/navigation/Navigation';
 import classes from './Rout.module.css';
 
 const RoutLayout = () => {
   return (
-    <>
-      <Navigation />
+    <div className={classes.layoutLocalPickup}>
+      <header className={classes.header}>
+        <Navigation />
+      </header>
       <main className={classes.content}>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
